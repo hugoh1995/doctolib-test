9fe23cbfffd343171d26053b1931c119283dc5b3
Hugo Houyez <hugohconcordia@gmail.com>
2018-08-29 18:37:04 +0200

    add detailed description for the edge cases

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,15 +3,10 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
 
   test "testing events" do
-
     Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
     Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
-
-    Event.create kind: 'opening', starts_at: DateTime.parse("2012-08-15 14:30"), ends_at: DateTime.parse("2012-08-15 16:00"), weekly_recurring: true
-    Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-13 15:30"), ends_at: DateTime.parse("2014-08-13 16:00")
-    Event.create kind: 'opening', starts_at: DateTime.parse("2012-08-16 13:30"), ends_at: DateTime.parse("2012-08-16 10:00"), weekly_recurring: true
-
     availabilities = Event.availabilities DateTime.parse("2014-08-10")
+
     assert_equal Date.new(2014, 8, 10), availabilities[0][:date]
     assert_equal [], availabilities[0][:slots]
     assert_equal Date.new(2014, 8, 11), availabilities[1][:date]
@@ -19,10 +14,29 @@ class EventTest < ActiveSupport::TestCase
     assert_equal [], availabilities[2][:slots]
     assert_equal Date.new(2014, 8, 16), availabilities[6][:date]
     assert_equal 7, availabilities.length
+  end
+
+  test "starts_at should be before ends_at" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
+    Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
+    availabilities = Event.availabilities DateTime.parse("2014-08-10")
 
     assert_operator availabilities[1][:slots].last.to_time, :>, availabilities[1][:slots].first.to_time
-    assert_equal ["14:30", "15:00"], availabilities[3][:slots]
+  end
+
+  test "events with wrong time format should be invalid" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2012-08-16 13:30"), ends_at: DateTime.parse("2012-08-16 10:00"), weekly_recurring: true
+    availabilities = Event.availabilities DateTime.parse("2014-08-10")
+
     assert_equal [], availabilities[6][:slots]
+  end
+
+  test "recuring openings should work on a recuring basis" do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2012-08-15 14:30"), ends_at: DateTime.parse("2012-08-15 16:00"), weekly_recurring: true
+    Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-13 15:30"), ends_at: DateTime.parse("2014-08-13 16:00")
+    availabilities = Event.availabilities DateTime.parse("2014-08-10")
+
+    assert_equal ["14:30", "15:00"], availabilities[3][:slots]
   end
 
 end
